72cfc9719d7cee85b2118244c2149ba9a2259393
hanuri5155 <hanuri5155@gmail.com>
2024-12-04 16:20:42 +0900

    프론트 수정(네비게이션 바 애니메이션 제거)

--- src/main/resources/static/js/navbar_animation.js
+++ src/main/resources/static/js/navbar_animation.js
@@ -1,104 +1,61 @@
 function test() {
+    // 네비게이션 바 요소 전체 선택
     var tabsNewAnim = $("#navbarSupportedContent");
-    var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+
+    // 현재 활성화된(active) 항목 선택
     var activeItemNewAnim = tabsNewAnim.find(".active");
+
+    // 활성화된 항목의 높이와 너비 구하기
     var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
     var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+
+    // 활성화된 항목의 위치 정보 가져오기 (top, left 값 포함)
     var itemPosNewAnimTop = activeItemNewAnim.position();
     var itemPosNewAnimLeft = activeItemNewAnim.position();
+
+    // 강조 표시 위치 설정
     $(".hori-selector").css({
         top: itemPosNewAnimTop.top + "px",
         left: itemPosNewAnimLeft.left + "px",
         height: activeWidthNewAnimHeight + "px",
         width: activeWidthNewAnimWidth + "px"
     });
-    $("#navbarSupportedContent").on("click", "li", function (e) {
-        $("#navbarSupportedContent ul li").removeClass("active");
-        $(this).addClass("active");
-        var activeWidthNewAnimHeight = $(this).innerHeight();
-        var activeWidthNewAnimWidth = $(this).innerWidth();
-        var itemPosNewAnimTop = $(this).position();
-        var itemPosNewAnimLeft = $(this).position();
-        $(".hori-selector").css({
-            top: itemPosNewAnimTop.top + "px",
-            left: itemPosNewAnimLeft.left + "px",
-            height: activeWidthNewAnimHeight + "px",
-            width: activeWidthNewAnimWidth + "px"
-        });
-    });
 }
+
+// 페이지 로드 후 실행되도록 설정
 $(document).ready(function () {
-    setTimeout(function () {
-        test();
-    });
+    // 페이지 로드 후 test 함수 호출하여 활성화된 항목의 기본 위치 설정하기
+    var activePage = localStorage.getItem("activePage");
+
+    // 이전에 클릭된 항목이 있는 경우 해당 항목에 활성화(active) 클래스 추가하기
+    if (activePage) {
+        $("#navbarSupportedContent ul li").removeClass("active");
+        $(`#navbarSupportedContent ul li a[data-page="${activePage}"]`).parent().addClass("active");
+    }
+
+    // 강조 표시 설정
+    test();
 });
+
+// 윈도우 리사이즈될 때 강조 표시 위치 재설정하기
 $(window).on("resize", function () {
     setTimeout(function () {
-        test();
+        test(); // 윈도우 크기 변경 시 강조 표시 위치 및 크기 재설정
     }, 500);
 });
-$(".navbar-toggler").click(function () {
-    $(".navbar-collapse").slideToggle(300);
-    setTimeout(function () {
-        test();
-    });
+
+// 네비게이션 바의 li 항목 클릭 시 강조 표시 이동 설정하기
+$("#navbarSupportedContent").on("click", "li", function () {
+    // 모든 li 항목에서 active 클래스 제거하기
+    $("#navbarSupportedContent ul li").removeClass("active");
+
+    // 클릭된 항목에 active 클래스 추가하기
+    $(this).addClass("active");
+
+    // 클릭된 페이지 정보 저장 (localStorage에 저장하여 새로고침 시 유지)
+    var page = $(this).find("a").data("page");
+    localStorage.setItem("activePage", page);
+
+    // 강조 표시 이동 설정하기
+    test();
 });
-
-// --------------add active class-on another-page move----------
-// jQuery(document).ready(function ($) {
-//     // Get current path and find target link
-//     var path = window.location.pathname.split("/").pop();
-//
-//     // Account for home page with empty path
-//     if (path == "") {
-//         path = "index.html";
-//     }
-//
-//     //var target = $('#navbarSupportedContent ul li a[th:href="@{' + path + '}"]');
-//     // Add active class to target link
-//     //target.parent().addClass("active");
-// });
-
-// jQuery(document).ready(function ($) {
-//     // 현재 페이지를 식별하기 위한 page 변수를 사용
-//     var currentPage = $('body').data('page');
-//
-//     // 페이지가 로드되었을 때 현재 페이지에 맞는 네비게이션 항목에 active 클래스를 추가
-//     $('#navbarSupportedContent ul li a').each(function () {
-//         var page = $(this).data('page');
-//         if (page === currentPage) {
-//             $(this).parent().addClass("active");
-//         }
-//     });
-//
-//     // 네비게이션 항목 클릭 이벤트 처리 (AJAX로 콘텐츠 변경)
-//     $('#navbarSupportedContent ul li a').click(function (e) {
-//         e.preventDefault(); // 기본 링크 동작 방지
-//
-//         var page = $(this).data('page'); // 클릭된 링크의 data-page 값 가져오기
-//
-//         // 네비게이션 항목의 active 클래스 업데이트
-//         $('#navbarSupportedContent ul li').removeClass("active");
-//         $(this).parent().addClass("active");
-//
-//         // 콘텐츠 영역 업데이트
-//         $.ajax({
-//             url: '/getContent', // 서버에서 콘텐츠를 제공하는 엔드포인트
-//             method: 'GET',
-//             data: { page: page }, // 서버에 요청 데이터로 page 전달
-//             success: function (response) {
-//                 // 성공적으로 데이터를 받으면 #content-area에 삽입
-//                 $('#content-area').html(response);
-//
-//                 // 탭 이동 시 애니메이션 적용
-//                 setTimeout(function () {
-//                     test(); // hori-selector 업데이트
-//                 });
-//             },
-//             error: function () {
-//                 // 에러 처리
-//                 alert('콘텐츠를 로드하는 중 오류가 발생했습니다.');
-//             }
-//         });
-//     });
-// });
